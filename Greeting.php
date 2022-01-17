30e606262ef44ea3e12fb548d80f86ed95fddccd
ryotagonG <ryotapb715@gmail.com>
2022-01-17 20:14:10 +0900

    数値型

--- Greeting.php
+++ Greeting.php
@@ -1,4 +1,9 @@
 <?php
-$num;
 $num = 1;
-echo $num;
+$num01 = 2;
+$num_01 = 3;
+// $num-01 = 4;
+echo $num. "\n";
+echo $num01. "\n";
+echo $num_01. "\n";
+// echo $num-01. "\n";
---
+++ Greeting2.php
@@ -0,0 +1,7 @@
+<?php
+$num01 = 123;
+$num02 = 1.23;
+
+echo gettype($num01). "\n";
+echo gettype($num02). "\n";
+var_dump($num02). "\n";
